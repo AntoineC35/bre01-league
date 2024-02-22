1f6ef2b25378a56ab0f5cdea0d2f5e4385daf6ab
AntoineC35 <antoine.cormier@3wa.io>
2024-02-22 13:47:56 +0100

    switching on pc

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+// charge l'autoload de composer
+require "vendor/autoload.php";
+
+// charge le contenu du .env dans $_ENV
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
